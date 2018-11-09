9b015500828585a21ea092f64a3358c7e71780d6
Tim Windelschmidt <fionera@fionera.de>
2018-11-10 00:44:01 +0100

    Resetting the Repository for a clean start!

--- config/initializers/database.cr
+++
@@ -1,5 +0,0 @@
-require "granite/adapter/sqlite"
-
-Granite::Adapters << Granite::Adapter::Sqlite.new({name: "sqlite", url: Amber.settings.database_url})
-Granite.settings.logger = Amber.settings.logger.dup
-Granite.settings.logger.progname = "Granite"
--- src/controllers/home_controller.cr
+++
@@ -1,8 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @page = "Home"
-    @url = "https://yivesmirror.com/"
-    render("index.ecr")
-  end
-end
-
