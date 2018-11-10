4d36bfc434aa08b5e759fce2420b1f481d9e3361
Tim Windelschmidt <fionera@fionera.de>
2018-11-10 03:27:16 +0100

    Add API

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,18 +2,88 @@
 
 namespace App\Controller;
 
+use App\Applications\ApplicationInterface;
+use App\Service\ApplicationService;
+use App\Service\BuildsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/api", name="api")
+ */
 class ApiController extends AbstractController
 {
     /**
-     * @Route("/api", name="api")
+     * @var ApplicationService
      */
-    public function index()
+    private $applicationService;
+    /**
+     * @var BuildsService
+     */
+    private $buildsService;
+
+    /**
+     * ApiController constructor.
+     * @param ApplicationService $applicationService
+     * @param BuildsService $buildsService
+     */
+    public function __construct(ApplicationService $applicationService, BuildsService $buildsService)
     {
-        return $this->render('api/index.html.twig', [
-            'title' => 'Api',
-        ]);
+        $this->applicationService = $applicationService;
+        $this->buildsService = $buildsService;
+    }
+
+
+    /**
+     * @Route("/list/{option?all}/{fileName}", name="list")
+     * @param string $option
+     * @param null|string $fileName
+     * @return JsonResponse
+     */
+    public function list(string $option, ?string $fileName = null): JsonResponse
+    {
+        $response = null;
+
+        if ($option === 'all') {
+            $response = $this->getAll();
+        } else if (\in_array($option, $this->getAll(), true)) {
+            if ($fileName !== null) {
+                $response = $this->getForBuild($option, $fileName);
+            } else {
+                $response = $this->getForApplication($option);
+            }
+        }
+
+        if ($response === null) {
+            throw $this->createNotFoundException('Unknown Option');
+        }
+
+        return new JsonResponse($response);
+    }
+
+    private function getForBuild(string $option, string $fileName): array
+    {
+        $application = $this->applicationService->getApplication($option);
+
+        if (!$this->buildsService->doesBuildExist($application, $fileName)) {
+            throw $this->createNotFoundException(sprintf('Could not find File %s for Application %s', $fileName, $option));
+        }
+
+        return $this->buildsService->getBuildForApplication($application, $fileName);
+    }
+
+    private function getForApplication(string $applicationName)
+    {
+        return array_map(function (array $build) {
+            return $build['fileName'];
+        }, $this->buildsService->getBuildsForApplication($this->applicationService->getApplication($applicationName)));
+    }
+
+    private function getAll()
+    {
+        return array_map(function (ApplicationInterface $application) {
+            return $application->getName();
+        }, $this->applicationService->getApplications());
     }
 }
--- src/Service/BuildsService.php
+++ src/Service/BuildsService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use App\Applications\ApplicationInterface;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 use Symfony\Component\Routing\RouterInterface;
 
 class BuildsService
@@ -23,7 +24,7 @@ class BuildsService
         $this->router = $router;
     }
 
-    public function getBuildsForApplication(ApplicationInterface $application)
+    public function getBuildsForApplication(ApplicationInterface $application): array
     {
         $applicationPath = $this->getPathForApplication($application);
 
@@ -32,58 +33,100 @@ class BuildsService
 
         $builds = [];
         foreach ($finder as $file) {
-            $versionRegex = $application->getVersionRegex();
-
-            if (!\is_array($versionRegex)) {
-                $versionRegex = [$versionRegex];
-            }
-
-            $version = '';
-            foreach ($versionRegex as $regex) {
-                preg_match($regex, $file->getFilename(), $version);
-
-                if (isset($version[1])) {
-                    $version = $version[1];
-                    break;
-                }
-            }
-
-            if (\is_array($version)) {
-                $version = $file->getBasename();
-            }
-
-            $builds[] = [
-                'fileName' => $file->getFilename(),
-                'version' => $version,
-                'size' => $file->getSize(),
-                'date' => date('Y-m-d', $file->getMTime()),
-                'downloadUrl' => $this->router->generate('files', [
-                    'applicationName' => $application->getName(),
-                    'fileName' => $file->getFilename(),
-                ]),
-            ];
+            $builds[] = $this->getBuildForFile($application, $file);
         }
 
         return $builds;
     }
 
+    public function getBuildForApplication(ApplicationInterface $application, string $fileName): array
+    {
+        return $this->getBuildForFile($application, $this->getSplFile($application, $fileName));
+    }
+
+    private function getBuildForFile(ApplicationInterface $application, SplFileInfo $file): array
+    {
+        $versionRegex = $application->getVersionRegex();
+
+        if (!\is_array($versionRegex)) {
+            $versionRegex = [$versionRegex];
+        }
+
+        $version = '';
+        foreach ($versionRegex as $regex) {
+            preg_match($regex, $file->getFilename(), $version);
+
+            if (isset($version[1])) {
+                $version = $version[1];
+                break;
+            }
+        }
+
+        if (\is_array($version)) {
+            $version = $file->getBasename();
+        }
+
+        return [
+            'fileName' => $file->getFilename(),
+            'version' => $version,
+            'size' => $file->getSize(),
+            'date' => date('Y-m-d', $file->getMTime()),
+            'downloadUrl' => $this->router->generate('files', [
+                'applicationName' => $application->getName(),
+                'fileName' => $file->getFilename(),
+            ], RouterInterface::ABSOLUTE_URL),
+
+
+            'size_bytes' => $file->getSize(),
+            'size_human' => $this->getHumanFilesize($file->getSize()),
+            'date_epoch' => $file->getMTime(),
+            'date_human' => date('F d, Y', $file->getMTime()),
+            'mc_version' => $version,
+            'direct_link' => $this->router->generate('files', [
+                'applicationName' => $application->getName(),
+                'fileName' => $file->getFilename(),
+            ], RouterInterface::ABSOLUTE_URL),
+            'grab_link' => $this->router->generate('grab', [
+                'applicationName' => $application->getName(),
+                'fileName' => $file->getFilename(),
+            ], RouterInterface::ABSOLUTE_URL),
+        ];
+    }
+
     public function getPathForBuild(ApplicationInterface $application, string $fileName): string
     {
-        return $this->getPathForApplication($application).DIRECTORY_SEPARATOR.$fileName;
+        return $this->getPathForApplication($application) . DIRECTORY_SEPARATOR . $fileName;
     }
 
     public function getPathForApplication(ApplicationInterface $application): string
     {
-        return getenv('DATA_PATH').DIRECTORY_SEPARATOR.$application->getName();
+        return getenv('DATA_PATH') . DIRECTORY_SEPARATOR . $application->getName();
     }
 
     public function doesBuildExist(ApplicationInterface $application, string $fileName): bool
+    {
+        return $this->getSplFile($application, $fileName) !== null;
+    }
+
+    private function getSplFile(ApplicationInterface $application, string $fileName): ?SplFileInfo
     {
         $applicationPath = $this->getPathForApplication($application);
 
         $finder = new Finder();
         $finder->files()->in($applicationPath)->name($fileName);
 
-        return 1 === $finder->count();
+        if ($finder->count() === 1) {
+            foreach ($finder as $file) {
+                return $file;
+            }
+        }
+
+        return null;
+    }
+
+    private function getHumanFilesize($bytes, $decimals = 2) {
+        $sz = 'BKMGTP';
+        $factor = (int) floor((\strlen($bytes) - 1) / 3);
+        return sprintf("%.{$decimals}f", $bytes / (1024 ** $factor)) . @$sz[$factor];
     }
 }
